40d2e1330c3a4f4a52745519f7c65e5149364054
amelialearn <amelia.learner@gmail.com>
2023-04-18 16:47:41 -0400

    made movie changes

--- Database.py
+++ Database.py
@@ -17,13 +17,14 @@ movies = response.json()
 for movie in movies:
     title = movie["Title"]
     year = int(movie["Year"])
+    genre = movie["Genre"]
     rating = float(movie["imdbRating"])
     box_office = movie["BoxOffice"]
 
     conn = sqlite3.connect('movies.db')
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, year INTEGER, rating FLOAT, box_office TEXT);")
-    cur.execute("INSERT INTO movies (title, year, rating, box_office) VALUES (?, ?, ?, ?, ?)", (title, year, rating, box_office))
+    cur.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, year INTEGER, genre TEXT, rating FLOAT, box_office TEXT);")
+    cur.execute("INSERT INTO movies (title, year, rating, box_office) VALUES (?, ?, ?, ?, ?, ?)", (title, year, genre, rating, box_office))
     conn.commit()
 conn.close()
 

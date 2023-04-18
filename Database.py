d052931517a48a557354099a0e7bb2d22f6a4584
amelialearn <amelia.learner@gmail.com>
2023-04-18 16:06:27 -0400

    wrote imports

--- Database.py
+++ Database.py
@@ -2,3 +2,11 @@
 #Partners: Amelia Learner, Jackson Gelbard, and Jackson Gertner
 
 #API DATABASE FILE
+
+import requests
+import json
+import sqlite3
+import os
+
+url = "http://www.omdbapi.com/?apikey=99b5ee6f&"
+response = requests.get(url)
--- visualizations.py
+++ visualizations.py
@@ -2,3 +2,9 @@
 #Partners: Amelia Learner, Jackson Gelbard, and Jackson Gertner
 
 #VISUALIZATIONS
+
+import requests
+import json
+import sqlite3
+import os
+import matplotlib.pyplot as plt

41566da1e06bd36b234cb2954cc71a16df06a447
amelialearn <amelia.learner@gmail.com>
2023-04-18 16:35:53 -0400

    attempted movie db

--- Database.py
+++ Database.py
@@ -8,5 +8,25 @@ import json
 import sqlite3
 import os
 
-url = "http://www.omdbapi.com/?apikey=99b5ee6f&"
+#movie information
+movie_key = "99b5ee6f"
+url = f"http://www.omdbapi.com/?apikey={movie_key}&" #change the url to be from the top 100 movies in 21st century
 response = requests.get(url)
+movies = response.json()
+
+for movie in movies:
+    title = movie["Title"]
+    year = int(movie["Year"])
+    rating = float(movie["imdbRating"])
+    box_office = movie["BoxOffice"]
+
+    conn = sqlite3.connect('movies.db')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, year INTEGER, rating FLOAT, box_office TEXT);")
+    cur.execute("INSERT INTO movies (title, year, rating, box_office) VALUES (?, ?, ?, ?, ?)", (title, year, rating, box_office))
+    conn.commit()
+conn.close()
+
+#tv show information
+
+#twitter/google information

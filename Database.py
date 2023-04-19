0ebb6a7dd92d0e8dc43b131ca6b0a07829793ccc
amelialearn <amelia.learner@gmail.com>
2023-04-18 21:23:43 -0400

    changes to movie code

--- Database.py
+++ Database.py
@@ -10,26 +10,28 @@ import os
 
 def movie():
     movie_key = "99b5ee6f"
-    url = f"http://www.omdbapi.com/?apikey={movie_key}&"
+    url = f"http://www.omdbapi.com/?apikey={movie_key}&type=movie&s=&y=&r=json&plot=short&tomatoes=true&certification=R&sort_by=imdbRating&desc=true"
     response = requests.get(url)
-    movies = response.text
-    d = json.loads(movies)
-    print(d)
+    movies = response.json()["Search"]
+    #d = json.loads(movies)
+    print(movies)
 
-    for i in range(25):
-        for movie in movies:
-            title = movie["Title"]
-            year = int(movie["Year"])
-            genre = movie["Genre"]
-            rating = float(movie["imdbRating"])
-            box_office = movie["BoxOffice"]
+    # for i in range(25):
+    #     for movie in movies:
+    #         title = movie["Title"]
+    #         year = int(movie["Year"])
+    #         genre = movie["Genre"]
+    #         rating = float(movie["imdbRating"])
+    #         box_office = movie["BoxOffice"]
 
-            conn = sqlite3.connect('movies.db')
-            cur = conn.cursor()
-            cur.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, year INTEGER, genre TEXT, rating FLOAT, box_office TEXT);")
-            cur.execute("INSERT INTO movies (title, year, rating, box_office) VALUES (?, ?, ?, ?, ?, ?)", (title, year, genre, rating, box_office))
-            conn.commit()
-    conn.close()
+    #         conn = sqlite3.connect('movies.db')
+    #         cur = conn.cursor()
+    #         cur.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, year INTEGER, genre TEXT, rating FLOAT, box_office TEXT);")
+    #         cur.execute("INSERT INTO movies (title, year, rating, box_office) VALUES (?, ?, ?, ?, ?, ?)", (title, year, genre, rating, box_office))
+    #         conn.commit()
+    # conn.close()
+
+movie()
 
 #tv show information
 

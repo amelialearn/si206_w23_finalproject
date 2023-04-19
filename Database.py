646a36d263f1389daad87ac0891d5e42d09c12a2
amelialearn <amelia.learner@gmail.com>
2023-04-18 20:47:06 -0400

    trying to fix url

--- Database.py
+++ Database.py
@@ -8,26 +8,28 @@ import json
 import sqlite3
 import os
 
-#movie information
-movie_key = "99b5ee6f"
-url = f"http://www.omdbapi.com/?apikey={movie_key}&groups=top_100&sort=user_rating,desc" #change the url to be from the top 100 movies in 21st century
-response = requests.get(url)
-movies = response.json()
+def movie():
+    movie_key = "99b5ee6f"
+    url = f"http://www.omdbapi.com/?apikey={movie_key}&"
+    response = requests.get(url)
+    movies = response.text
+    d = json.loads(movies)
+    print(d)
 
-for i in range(25):
-    for movie in movies:
-        title = movie["Title"]
-        year = int(movie["Year"])
-        genre = movie["Genre"]
-        rating = float(movie["imdbRating"])
-        box_office = movie["BoxOffice"]
+    for i in range(25):
+        for movie in movies:
+            title = movie["Title"]
+            year = int(movie["Year"])
+            genre = movie["Genre"]
+            rating = float(movie["imdbRating"])
+            box_office = movie["BoxOffice"]
 
-        conn = sqlite3.connect('movies.db')
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, year INTEGER, genre TEXT, rating FLOAT, box_office TEXT);")
-        cur.execute("INSERT INTO movies (title, year, rating, box_office) VALUES (?, ?, ?, ?, ?, ?)", (title, year, genre, rating, box_office))
-        conn.commit()
-conn.close()
+            conn = sqlite3.connect('movies.db')
+            cur = conn.cursor()
+            cur.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, year INTEGER, genre TEXT, rating FLOAT, box_office TEXT);")
+            cur.execute("INSERT INTO movies (title, year, rating, box_office) VALUES (?, ?, ?, ?, ?, ?)", (title, year, genre, rating, box_office))
+            conn.commit()
+    conn.close()
 
 #tv show information
 

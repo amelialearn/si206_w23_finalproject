b6918e74823345fceaa705e9946166e1ac3c7ce1
amelialearn <amelia.learner@gmail.com>
2023-04-20 10:55:20 -0400

    code works but can't harcode list

--- Database.py
+++ Database.py
@@ -11,26 +11,26 @@ import os
 def movies():
     conn = sqlite3.connect('movies.db')
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, genre TEXT, rating FLOAT, box_office TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, runtime TEXT, genre TEXT, rating FLOAT, box_office TEXT)")
     
-    movie_key = "99b5ee6f"
-    url = f"http://www.omdbapi.com/?apikey={movie_key}&type=movie&s&y=&r=json&plot=full&page=1"
-    response = requests.get(url)
-    data = response.json()
+    list_movies = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "Schindler's List", "The Lord of the Rings: The Return of the King", "12 Angry Men", "The Godfather Part II", "Pulp Fiction", "Inception", "The Lord of the Rings: The Fellowship of the Ring", "Fight Club"]
+    all_movies = []
 
-    movies = data["Search"]
-    for movie in movies:
-        id = movie["imdbID"]
-        new_url = f"http://www.omdbapi.com/?apikey={movie_key}&i={id}&plot=full"
-        new_response = requests.get(new_url)
-        each_movie = new_response.json()
+    for item in list_movies:
+        movie_key = "99b5ee6f"
+        url = f"http://www.omdbapi.com/?t={item}&apikey={movie_key}"
+        response = requests.get(url)
+        data = response.json()
+        all_movies.append(data)
 
-        title = each_movie["Title"]
-        year = int(each_movie["Year"])
-        genre = each_movie["Genre"]
-        rating = float(each_movie["imdbRating"])
-        box_office = each_movie["BoxOffice"]
-        cur.execute("INSERT INTO movies (title, year, genre, rating, box_office) VALUES (?, ?, ?, ?, ?)", (title, year, genre, rating, box_office))
+    for movie in all_movies:
+        title = movie["Title"]
+        year = int(movie["Year"])
+        runtime = movie["Runtime"]
+        genre = movie["Genre"]
+        rating = float(movie["imdbRating"])
+        box_office = movie["BoxOffice"]
+        cur.execute("INSERT INTO movies (title, year, runtime, genre, rating, box_office) VALUES (?, ?, ?, ?, ?, ?)", (title, year, runtime, genre, rating, box_office))
         conn.commit()
 
     conn.close()

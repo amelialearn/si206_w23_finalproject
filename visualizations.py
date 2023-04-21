89faa1366167580e2d0ef774c62de606c9f889fb
amelialearn <amelia.learner@gmail.com>
2023-04-20 21:47:36 -0400

    finished one calculations

--- visualizations.py
+++ visualizations.py
@@ -8,3 +8,71 @@ import json
 import sqlite3
 import os
 import matplotlib.pyplot as plt
+
+def calc():
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + '/' + "TOP100.db")
+    cur = conn.cursor()
+
+    #average length over a certain period of time
+    #cur.execute("SELECT songs.runtime, movies.runtime, tv_shows.runtime, songs.year, movies.year, tv_shows.year FROM movies INNER JOIN tv_shows ON restaurants.building_id = buildings.id INNER JOIN categories ON restaurants.category_id = categories.id")
+    #rows = cur.fetchall()
+
+    #average imdb rating for movies vs tv shows for three decades (1990s, 2000s, 2010s)
+    cur.execute("SELECT movies.rating, show_info.imdb_rating, movies.year, tv_shows.year FROM show_info INNER JOIN movies ON show_info.key_ID = movies.id INNER JOIN tv_shows ON show_info.key_ID = tv_shows.key_ID")
+    rows = cur.fetchall() #movie rating, show rating, movie year, show year
+    d = {}
+    mcount1990 = 0
+    mtotal1990 = 0
+    mcount2000 = 0
+    mtotal2000 = 0
+    mcount2010 = 0
+    mtotal2010 = 0
+
+    tcount1990 = 0
+    ttotal1990 = 0
+    tcount2000 = 0
+    ttotal2000 = 0
+    tcount2010 = 0
+    ttotal2010 = 0
+    for item in rows:
+        if "199" in str(item[2]):
+            mcount1990 += 1
+            mtotal1990 += item[0]
+        elif "200" in str(item[2]):
+            mcount2000 += 1
+            mtotal2000 += item[0]
+        elif "201" in str(item[2]):
+            mcount2010 += 1
+            mtotal2010 += item[0]
+        elif "199" in str(item[3]):
+            tcount1990 += 1
+            ttotal1990 += float(item[1])
+        elif "200" in str(item[3]):
+            tcount2000 += 1
+            ttotal2000 += float(item[1])
+        elif "201" in str(item[3]):
+            tcount2010 += 1
+            ttotal2010 += float(item[1])
+    mavg1990  = mtotal1990/mcount1990
+    mavg2000  = mtotal2000/mcount2000
+    mavg2010  = mtotal2010/mcount2010
+
+    tavg1990  = ttotal1990/tcount1990
+    tavg2000  = ttotal2000/tcount2000
+    tavg2010  = ttotal2010/tcount2010
+        
+
+    #comparing imdb rating to runtime for movies an tv shows
+
+    #average imdb rating based on genre
+
+calc()
+
+def write_data():
+    pass
+
+#songs
+#movies
+#tv_shows - name and runtime and year
+#show_info - imdb rating
